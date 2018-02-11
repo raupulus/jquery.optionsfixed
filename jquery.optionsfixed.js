b5aedaf8d683917408c73529dcb57d4baa3bb7c9
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-12 00:32:29 +0100

    Se añaden todos los estilos y bloques para ser gestionados dinámicamente por el plugin

--- jquery.optionsfixed.js
+++ jquery.optionsfixed.js
@@ -4,15 +4,15 @@
      */
     $.optionsfixed = function(opciones) {
         var conf = {
-            imagen: '',
             menu: [],
-            background: '',
+            img: '',
+            back: '',
             left: '5px',
-            top: '5px'
+            top: '5px',
+            opacity: 0.6,
         };
 
-        // Reemplaza los valores introducido por el usuario en la
-        // configuracion establecida por defecto.
+        // Reemplaza los valores introducido por el usuario
         $.extend(conf, opciones);
 
         // Añadir al elemento "body" la caja con el lanzador
@@ -26,6 +26,32 @@
             'left' : conf.left,
             'top' : conf.top,
         });
+
+        // Estilos para la caja "boxFixed"
+        $('#boxFixed').css({
+            'position' : 'fixed',
+            'display' : 'block',
+            'opacity' : 0.6,
+            'width' : '50px',
+            'height' : '50px',
+            'cursor' : 'pointer',
+            'background' : conf.back + 'url("' + conf.img + '") no-repeat',
+            'background-size' : 'contain',
+            'border-radius' : '10px',
+        });
+
+        $('#boxFixed').hover(
+            function() {
+                $(this).css({
+                   'opacity' : 1,
+                });
+            },
+            function() {
+                $(this).css({
+                    'opacity' : conf.opacity,
+                });
+            }
+        );
 
         return $(this);
     };
