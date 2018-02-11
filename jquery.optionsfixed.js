c748d92f62876e74e37a978ee2722d5de7016426
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-12 00:15:31 +0100

    Amplio configuración y estilos para bloque padre

--- jquery.optionsfixed.js
+++ jquery.optionsfixed.js
@@ -2,16 +2,30 @@
     /**
      *
      */
-    $.fn.menuDesplegable = function(opciones) {
+    $.optionsfixed = function(opciones) {
         var conf = {
-            a: 'a'
+            imagen: '',
+            menu: [],
+            background: '',
+            left: '5px',
+            top: '5px'
         };
 
         // Reemplaza los valores introducido por el usuario en la
         // configuracion establecida por defecto.
         $.extend(conf, opciones);
 
+        // Añadir al elemento "body" la caja con el lanzador
+        $('body').append('<div id="boxFixedParent">' +
+                            '<div id="boxFixed"></div>' +
+                         '</div>');
 
+        // Estilos para la caja "boxFixedParent"
+        $('#boxFixedParent').css({
+            'position' : 'absolute',
+            'left' : conf.left,
+            'top' : conf.top,
+        });
 
         return $(this);
     };
